ca7611073678704d42a547192304acbde72a841c
syifaazh <meutuahsyifa409@gmail.com>
2024-06-06 10:26:30 +0700

    update logo

--- resources/views/layouts/mahasiswa/sidebar.blade.php
+++ resources/views/layouts/mahasiswa/sidebar.blade.php
@@ -4,7 +4,7 @@
     <!-- Sidebar - Brand -->
     <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
         <div class="sidebar-brand-text mx-3">
-            <img src="{{asset('assets/img/LOGO.png')}}" alt="logo" style="width: 150px;">
+            <img src="{{asset('assets/img/logo.png')}}" alt="logo" style="width: 150px;">
         </div>
     </a>
 
